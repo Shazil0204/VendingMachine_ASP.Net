4831c217841d03e60c27d34e8398f5ef449cb916
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-11-06 09:46:35 +0100

    Update TEMP.sql

--- TEMP/TEMP.sql
+++ TEMP/TEMP.sql
@@ -17,20 +17,20 @@ BEGIN
         SELECT 'THIS VENDING MACHINE ALREADY EXISTS';
     ELSEIF EXISTS (SELECT 1 FROM Cities WHERE City = CITY) THEN
         -- INSERT THIS DATABASE NAME INSIDE THE TABLE TO SHOW WHICH CITY HAS A VENDING MACHINE
-        SET v_SQL = CONCAT('INSERT INTO DBRecords(DBName) VALUES(''', CITY, '_VM'')');
+        SET v_SQL = CONCAT('INSERT INTO DBRecords(DBName) VALUES(', QUOTE(CONCAT(CITY, '_VM')), ')');
         PREPARE stmt FROM v_SQL;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
 
         -- Create the new database
-        SET v_SQL = CONCAT('CREATE DATABASE ', QUOTE(CITY, '_VM'));
+        SET v_SQL = CONCAT('CREATE DATABASE `', CITY, '_VM`');
         PREPARE stmt FROM v_SQL;
         EXECUTE stmt;
         DEALLOCATE PREPARE stmt;
 
         -- Create tables in the new database and insert data
         SET v_SQL = CONCAT(
-        'USE ', QUOTE(CITY, '_VM'), ';
+        'USE `', CITY, '_VM`;
 
         -- [rest of your table creation and data insertion logic goes here]
         ');

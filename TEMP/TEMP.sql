8f9f47f620277c3d9fbeffd102f5a5361e0a6058
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-11-06 12:19:55 +0100

    Update TEMP.sql

--- TEMP/TEMP.sql
+++ TEMP/TEMP.sql
@@ -1,79 +1,31 @@
 DELIMITER //
 
-CREATE PROCEDURE CREATING_VM(IN CITY VARCHAR(100), OUT RESULT INT)
+CREATE PROCEDURE READ_VM(IN DBName VARCHAR(50), OUT RESULT INT)
 BEGIN
     DECLARE v_SQL TEXT;
 
-    -- Initialize RESULT to a default value
-    SET RESULT = -3;
-
-    -- Check if the database already exists in MainDB
+    -- Check if the database exists
     IF EXISTS (
         SELECT schema_name 
         FROM information_schema.schemata 
-        WHERE schema_name = CONCAT(CITY, '_VM')
+        WHERE schema_name = DBName
     ) THEN
-        SET RESULT = -2;
-        SELECT 'THIS VENDING MACHINE ALREADY EXISTS';
-    ELSEIF EXISTS (SELECT 1 FROM Cities WHERE City = CITY) THEN
-        -- INSERT THIS DATABASE NAME INSIDE THE TABLE TO SHOW WHICH CITY HAS A VENDING MACHINE
-        SET v_SQL = CONCAT('INSERT INTO DBRecords(DBName) VALUES(', QUOTE(CONCAT(CITY, '_VM')), ')');
-        PREPARE stmt FROM v_SQL;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;
+        -- Use the database and fetch the required data
+        SET v_SQL = CONCAT('USE `', DBName, '`;
 
-        -- Create the new database
-        SET v_SQL = CONCAT('CREATE DATABASE `', CITY, '_VM`');
-        PREPARE stmt FROM v_SQL;
-        EXECUTE stmt;
-        DEALLOCATE PREPARE stmt;
-
-        -- Create tables in the new database and insert data
-        SET v_SQL = CONCAT(
-        'USE `', CITY, '_VM`;
-
-        CREATE TABLE Brands(
-            BrandID INT AUTO_INCREMENT PRIMARY KEY,
-            ProductBrand VARCHAR(30) NOT NULL
-        );
-
-        CREATE TABLE Types(
-            TypeID INT AUTO_INCREMENT PRIMARY KEY,
-            ProductType VARCHAR(30)
-        );
-
-        CREATE TABLE Products(
-            ProductID int AUTO_INCREMENT PRIMARY KEY,
-            ProductName VARCHAR(30),
-            ProductURL VARCHAR(1000),
-            Stock int,
-            Price INT NOT NULL,
-            BrandID INT NOT NULL,
-            TypeID INT NOT NULL,
-            FOREIGN KEY(BrandID) REFERENCES Brands(BrandID),
-            FOREIGN KEY(TypeID) REFERENCES Types(TypeID)
-        );
-
-        CREATE TABLE Transactions(
-            TransactionID INT AUTO_INCREMENT PRIMARY KEY,
-            TotalPrice INT NOT NULL,
-            Quantity INT NOT NULL,
-            ProductID INT NOT NULL,
-            FOREIGN KEY(ProductID) REFERENCES Products(ProductID)
-        );
-
-        INSERT INTO Brands (ProductBrand) VALUES (''BrandA''), (''BrandB''), (''BrandC'');
-
-        INSERT INTO Types (ProductType) VALUES (''Type1''), (''Type2''), (''Type3'');
-
-        INSERT INTO Products (ProductName, ProductURL, Stock, Price, BrandID, TypeID) 
-        VALUES 
-            (''ProductA1'', ''https://img.freepik.com/free-vector/realistic-hand-drawn-fuck-you-symbol_23-2148684365.jpg'', 100, 50, 1, 1),
-            (''ProductA2'', ''https://img.freepik.com/free-vector/realistic-hand-drawn-fuck-you-symbol_23-2148684365.jpg'', 150, 60, 2, 2),
-            (''ProductA3'', ''https://img.freepik.com/free-vector/realistic-hand-drawn-fuck-you-symbol_23-2148684365.jpg'', 200, 70, 3, 3),
-            (''ProductB1'', ''https://img.freepik.com/free-vector/realistic-hand-drawn-fuck-you-symbol_23-2148684365.jpg'', 110, 55, 1, 2),
-            (''ProductB2'', ''https://img.freepik.com/free-vector/realistic-hand-drawn-fuck-you-symbol_23-2148684365.jpg'', 155, 65, 2, 3),
-            (''ProductB3'', ''https://img.freepik.com/free-vector/realistic-hand-drawn-fuck-you-symbol_23-2148684365.jpg'', 210, 75, 3, 1);
+        SELECT
+            P.ProductName,
+            P.ProductURL,
+            P.Stock,
+            P.Price,
+            T.ProductType,
+            B.ProductBrand
+        FROM
+            Products AS P
+        INNER JOIN
+            Brands AS B ON P.BrandID = B.BrandID
+        INNER JOIN
+            Types AS T ON P.TypeID = T.TypeID;
         ');
 
         PREPARE stmt FROM v_SQL;
@@ -81,10 +33,10 @@ BEGIN
         DEALLOCATE PREPARE stmt;
 
         SET RESULT = 0; -- Indicating success
-        SELECT 'DONE SUCCESSFULLY';
+        SELECT 'HERE YOU GO!';
     ELSE
-        SET RESULT = -1; -- City name doesn't exist
-        SELECT 'THIS CITY NAME DOES NOT EXIST IN OUR DATABASE';
+        SET RESULT = -1; -- Database not found
+        SELECT 'CAN''T FIND A DATABASE WITH THIS NAME';
     END IF;
 END //
 

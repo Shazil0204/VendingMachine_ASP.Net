2dff923e22396997aa4b47ee17523d49fab626f7
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-11-06 12:25:07 +0100

    Update TEMP.sql

--- TEMP/TEMP.sql
+++ TEMP/TEMP.sql
@@ -1,6 +1,16 @@
 DELIMITER //
 
-CREATE PROCEDURE READ_VM(IN DBName VARCHAR(50), OUT RESULT INT)
+CREATE PROCEDURE UPDATE_VM(
+    IN DBName VARCHAR(50),
+    IN ProductID INT,
+    IN ProductName VARCHAR(30),
+    IN ProductURL TEXT,
+    IN Stock INT,
+    IN Price INT,
+    IN BrandID INT,
+    IN TypeID INT,
+    OUT RESULT INT
+)
 BEGIN
     DECLARE v_SQL TEXT;
 
@@ -10,33 +20,26 @@ BEGIN
         FROM information_schema.schemata 
         WHERE schema_name = DBName
     ) THEN
-        -- Use the database and fetch the required data
-        SET v_SQL = CONCAT('USE `', DBName, '`;
+        -- Prepare the update statement
+        SET v_SQL = CONCAT('UPDATE `', DBName, '`.Products SET 
+            ProductName = ?', 
+            ', ProductURL = ?', 
+            ', Stock = ?', 
+            ', Price = ?', 
+            ', BrandID = ?', 
+            ', TypeID = ? 
+        WHERE ProductID = ?');
 
-        SELECT
-            P.ProductName,
-            P.ProductURL,
-            P.Stock,
-            P.Price,
-            T.ProductType,
-            B.ProductBrand
-        FROM
-            Products AS P
-        INNER JOIN
-            Brands AS B ON P.BrandID = B.BrandID
-        INNER JOIN
-            Types AS T ON P.TypeID = T.TypeID;
-        ');
-
+        -- Execute the prepared statement with provided parameters
         PREPARE stmt FROM v_SQL;
-        EXECUTE stmt;
+        EXECUTE stmt USING ProductName, ProductURL, Stock, Price, BrandID, TypeID, ProductID;
         DEALLOCATE PREPARE stmt;
 
         SET RESULT = 0; -- Indicating success
-        SELECT 'HERE YOU GO!';
+        SELECT CONCAT('SYSTEM HAVE UPDATED TABLE IN DATABASE `', DBName, '` SUCCESSFULLY');
     ELSE
         SET RESULT = -1; -- Database not found
-        SELECT 'CAN''T FIND A DATABASE WITH THIS NAME';
+        SELECT 'SYSTEM CAN''T FIND A DATABASE WITH THIS NAME';
     END IF;
 END //
 

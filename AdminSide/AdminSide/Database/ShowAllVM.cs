fc2f8c2a737dc379019ca68b5dae3edda1d2b514
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-11-16 09:02:06 +0100

    update program

---
+++ AdminSide/AdminSide/Database/ShowAllVM.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace AdminSide.Database
+{
+	public class ShowAllVM
+	{
+		public List<string> GetCities()
+		{
+			List<string> cities = new List<string>();
+
+			var config = new ConfigurationBuilder()
+			   .AddJsonFile("appsettings.json")
+			   .Build();
+
+			var conn = config.GetConnectionString("AllVendingMachinesDB");
+
+			using (SqlConnection connection = new SqlConnection(conn))
+			{
+				string query = "SELECT DBName FROM DBRecords";
+
+				SqlCommand command = new SqlCommand(query, connection);
+
+				try
+				{
+					connection.Open();
+
+					SqlDataReader reader = command.ExecuteReader();
+
+					while (reader.Read())
+					{
+						cities.Add(reader["DBName"].ToString());
+					}
+
+					reader.Close();
+				}
+				catch (SqlException e)
+				{
+					Console.WriteLine(e.Message);
+					// Handle exception
+				}
+			}
+
+			return cities;
+		}
+	}
+}
--- AdminSide/AdminSide/Pages/showAll.cshtml.cs
+++ AdminSide/AdminSide/Pages/showAll.cshtml.cs
@@ -5,8 +5,20 @@ namespace AdminSide.Pages
 {
     public class showAllModel : PageModel
     {
+
+		Database.ShowAllVM VM = new Database.ShowAllVM();
+
         public void OnGet()
         {
         }
-    }
+
+		public List<string> VMs()
+		{
+			List<string> vm = new List<string>();
+
+			vm = VM.GetCities();
+
+			return vm;
+		}
+	}
 }

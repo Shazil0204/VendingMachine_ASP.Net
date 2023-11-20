27642403cb93a93c8c706018d820ea6977e60c09
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-11-20 10:40:00 +0100

    update

--- AdminSide/AdminSide/obj/Debug/net7.0/AdminSide.AssemblyInfo.cs
+++ AdminSide/AdminSide/obj/Debug/net7.0/AdminSide.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdminSide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a6270b89da1ea1ca0c1790afdcea48c01a38945")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0a01d962ac08f2799ff610989e830d72739353a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdminSide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdminSide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
